3c4b3a3adb8d6150030944bd36291d706254bdb9
Hassan8521 <laazihassan8521>
2022-06-07 07:56:02 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/pyhton3
 from sys import argv
 if __name__ == "__main__":
-    arglen = len(argv) - 1
-    if arglen == 0:
-        print('0 arguments.')
-    elif arglen == 1:
-        print('1 argument:')
-        print('1: {}'.format(argv[1]))
-    else:
-        print('{} arguments:'.format(arglen))
-        for i in range(1, len(argv)):
-            print('{}: {}'.format(i, argv[i]))
+    newsum = 0
+    if len(argv) > 1:
+        for 1 in range(1, len(argv)):
+            newsum += int(argv[i])
+            print(newsum)
