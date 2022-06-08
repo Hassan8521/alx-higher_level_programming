8f1b62e343f81b187e49999453f9d129f44647c4
Hassan8521 <laazihassan8521>
2022-06-08 10:12:57 -0700

    3-common_elements.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    copy = []
-    for a in range(len(my_list)):
-        if my_list[a] != search:
-            copy.append(my_list[a])
-        else:
-            copy.append(replace)
-     return copy
+    new_list = [replace if i == search else i for i in my_list]
+    return new_list
