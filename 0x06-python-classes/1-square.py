4cb6fb6f79127156f16d8920ce7cc85f65370c13
Hassan8521 <laazihassan8521>
2022-06-22 04:52:52 -0700

    fix oystyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
-
 """Define a class Square"""
 
-class Square:
 
-    """class for square  """
-    
+class Square:
+    """class for a square"""
+
     def __init__(self, size):
-        """ initializing size.
+        """initializing size.
         Args:
-            
-            size (int): The size of square.
-        """
+             size (int): The size of square.
+         """
 
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """Define a square"""
 
+
 class Square:
     """class that define a square based o 2-square"""
 
-    
     def __init__(self, size=0):
         """initializing instant attribute size.
         Args:
