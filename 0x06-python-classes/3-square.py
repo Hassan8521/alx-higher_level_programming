f6b5334e2c06dfe9a689f6361d58abd8c86769c8
Hassan8521 <laazihassan8521>
2022-06-22 02:44:00 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bi/python3
+"""Define a square"""
+
+class Square:
+    """class that define a square based o 2-square"""
+
+    
+    def __init__(self, size=0):
+        """initializing instant attribute size.
+        Args:
+            size (int): the size of a square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
